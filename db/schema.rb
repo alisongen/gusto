c77493fd2c645ed459eb14c65b1877193d472cfb
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-03-11 15:35:27 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_11_103542) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_11_142557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "collections", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.bigint "saved_restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["saved_restaurant_id"], name: "index_collections_on_saved_restaurant_id"
+    t.index ["user_id"], name: "index_collections_on_user_id"
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.string "status"
@@ -76,6 +86,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_103542) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "collections", "saved_restaurants"
+  add_foreign_key "collections", "users"
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "reviews", "saved_restaurants"
