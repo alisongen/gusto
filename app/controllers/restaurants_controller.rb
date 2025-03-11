2009ae396e29f4a01ea85ff7da9e3a1b093b5b4d
Alison Genin <alison.genin@gmail.com>
2025-03-11 16:59:34 +0100

    restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,25 +1,9 @@
 class RestaurantsController < ApplicationController
   def index
-    Restaurant.all
+    @restaurants = Restaurant.all
   end
 
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def delete
-
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 end
