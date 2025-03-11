5035e0f4d8ee038d6ac110b6e11e10258e9a9ecd
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-03-11 16:56:35 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :saved_restaurants, except: :show do
-    resources :reviews, only: [:]
-  end
+  resources :saved_restaurants, except: :show
+
+  resources :friendships, except: %i[edit update]
+
+  get "dashboard", to: "pages#dashboard"
+  get "actuality", to: "pages#actuality"
 end
