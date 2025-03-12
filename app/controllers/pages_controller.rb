c8102db57932f6d4191dfde09c9be4aa33c8ef37
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-03-12 15:45:03 +0100

    etape

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @user = current_user
-    # @collection.user = @user
-    # @collection = Collection.find(name: "Favorites")
+    # J'assigne à ma variable d'instance "@restaurants" les restaurants correspondant à ma recherche
+    # si une valeur "query" est présente dans mes params
+    @restaurants = Restaurant.search_by_name(params[:query]) if params[:query].present?
   end
 end
