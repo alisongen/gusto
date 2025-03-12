7f8a098e511020acc03b37b72a2e391487f7a4d9
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-03-12 16:40:09 +0100

    homepage finie je pense

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,8 @@ class PagesController < ApplicationController
     # J'assigne à ma variable d'instance "@restaurants" les restaurants correspondant à ma recherche
     # si une valeur "query" est présente dans mes params
     @restaurants = Restaurant.search_by_name(params[:query]) if params[:query].present?
+
+    @user = User.find(12)
+    @saved_restaurants = @user.saved_restaurants
   end
 end
