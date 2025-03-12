e4cceb9dc76c00ef8cddc7832292912fca52bd5f
Alison Genin <alison.genin@gmail.com>
2025-03-12 16:28:31 +0100

    saved restaurant en cours

--- app/controllers/saved_restaurants_controller.rb
+++ app/controllers/saved_restaurants_controller.rb
@@ -1,25 +1,32 @@
 class SavedRestaurantsController < ApplicationController
-  def new
-
-  end
-
-  def create
-
-  end
-
   def index
     SavedRestaurant.all
   end
 
-  def edit
+  def new
+    @saved_restaurant = SavedRestaurant.new
+  end
 
+  def create
+    @user = current_user
+    @saved_restaurant = SavedRestaurant.new(saved_restaurant_params)
+    @saved_restaurant.user_id = @user.id
+    @saved_restaurant.restaurant_id = ??
+    @saved_restaurant.save
+  end
+
+  def edit
   end
 
   def update
-
   end
 
   def delete
+  end
 
+  private
+
+  def saved_restaurant_params
+    params.require(:saved_restaurant).permit(:restaurant_id, :user_id)
   end
 end
