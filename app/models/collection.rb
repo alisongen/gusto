c1c0b9a334add430c66c5c09e0fae7a025a947d2
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-03-12 11:27:27 +0100

    reupdate models and associations

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,4 @@
 class Collection < ApplicationRecord
   belongs_to :user
-  has_many :saved_restaurants_collections
+  has_many :saved_restaurants, through: :saved_restaurants_collections
 end
--- app/models/saved_restaurant.rb
+++ app/models/saved_restaurant.rb
@@ -1,5 +1,5 @@
 class SavedRestaurant < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
-  has_many :saved_restaurants_collections
+  has_many :collections, through: :saved_restaurants_collections
 end
