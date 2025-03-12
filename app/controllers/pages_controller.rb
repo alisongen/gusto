eb4e5cc94a83bc7ece1134c37678b22e3dff7b75
Gautier de MAUROY <gautier@de-mauroy.fr>
2025-03-12 16:11:02 +0100

    deuxième partie initialisation cloudinary

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @user = current_user
+    @saved_restaurant = current_user.saved_restaurants.first
     # @collection.user = @user
     # @collection = Collection.find(name: "Favorites")
   end
--- app/models/saved_restaurant.rb
+++ app/models/saved_restaurant.rb
@@ -2,6 +2,7 @@ class SavedRestaurant < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
 
+  has_many_attached :photo
   has_many :saved_restaurants_collections
   has_many :collections, through: :saved_restaurants_collections
 end
