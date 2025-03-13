c130b9affd5bfa4cd032d846586ddb970ae6240b
Gil DAYAN <gil.dayan@orange.fr>
2025-03-13 16:30:41 +0100

    correctif

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,11 +8,13 @@ class PagesController < ApplicationController
     # si une valeur "query" est présente dans mes params
     @restaurants = Restaurant.search_by_name(params[:query]) if params[:query].present?
     # J'assigne à ma variable d'instance "@user" le "current_user"
-    @user = current_user
-    # J'associe à ma variable "@saved_restaurants" les "saved_restaurants" de mon "@user"
-    @saved_restaurants = @user.saved_restaurants
-    # CODE TEST
-    @saved_restaurant = @saved_restaurants.first
+    if (user_signed_in?)
+      @user = current_user
+      # J'associe à ma variable "@saved_restaurants" les "saved_restaurants" de mon "@user"
+      @saved_restaurants = @user.saved_restaurants
+      # CODE TEST
+      @saved_restaurant = @saved_restaurants.first
+    end
   end
 
   def dashboard
