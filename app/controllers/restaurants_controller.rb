d84c1f4256bcd7a9ac90740e1b3636f29b9036a8
Gautier de MAUROY <gautier@de-mauroy.fr>
2025-03-13 13:07:18 +0100

    tentative infructueuse

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,10 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    photos = ['nick-karvounis-Ciqxn7FE4vE-unsplash_xitvdh', 'jason-leung-poI7DelFiVA-unsplash_elpnbh', 'volkan-vardar-1H30uRC1plc-unsplash_wksntl', 'jay-wennington-N_Y88TWmGwA-unsplash_rtdbef']
+    @photo = photos.sample
+    
+
     # @collections = Collection.where(user: current_user)
     # @collection = Collection.new
     # @collection.saved_restaurant = @restaurant
