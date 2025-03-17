5f4d4bea4b73c95543f40483214e093cc7f1289d
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-03-17 10:08:05 +0100

    finition des boutons catégories

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,6 +2,7 @@ class CollectionsController < ApplicationController
   def index
     @user = current_user
     @collections = @user.collections
+    @friendships = @user.friendships
 
     if params[:name].present?
       @restaurants = Collection.find_by(name: params[:name]).restaurants
