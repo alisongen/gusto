67a12bf3a8f32b42394efa89fb8d712ad0f8a08c
Gautier de MAUROY <gautier@de-mauroy.fr>
2025-03-18 11:52:34 +0100

    Update emoji selection in collections controller and adjust styles in new collection form

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -40,7 +40,7 @@ class CollectionsController < ApplicationController
       "#FF3380", # Hot Pink
       "#FF3366"  # Red
     ]
-    @emojis = ["🍕", "🍔", "🍟", "🌭", "🍿", "🍣", "🍜", "🍩", "🍪", "🍫", "🍦", "🍰"]
+    @emojis = [ "🍔", "🥞", "🌭", "🍿", "🍣", "🍜", "🍩", "🍪", "🍫", "🍦", "🍰"]
   end
 
   def create
