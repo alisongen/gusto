6c7f5e2d5026169ebc3ddb35b2193c267527c805
Gautier de MAUROY <gautier@de-mauroy.fr>
2025-03-18 10:09:24 +0100

    Add validations for color and emoji in Collection model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -4,4 +4,7 @@ class Collection < ApplicationRecord
   has_many :saved_restaurants_collections
   has_many :saved_restaurants, through: :saved_restaurants_collections
   has_many :restaurants, through: :saved_restaurants
+
+  validates :color, presence: true
+  validates :emoji, presence: true
 end
