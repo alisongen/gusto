5b5503dbabc55058251ed583a54c345362f13015
Gautier de MAUROY <gautier@de-mauroy.fr>
2025-03-18 13:27:41 +0100

    Add validation for collection name uniqueness and presence

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -5,6 +5,7 @@ class Collection < ApplicationRecord
   has_many :saved_restaurants, through: :saved_restaurants_collections
   has_many :restaurants, through: :saved_restaurants
 
+  validates :name, presence: true, uniqueness: { scope: :user_id, message: "You already have a collection with this name" }
   validates :color, presence: true
   validates :emoji, presence: true
 end
