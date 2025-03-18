3e4e3f98af257d6785cc8569164425b55fbb6e0b
Gautier de MAUROY <gautier@de-mauroy.fr>
2025-03-18 18:04:07 +0100

    Add authentication for dashboard and feed actions in PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: []
+  before_action :authenticate_user!, only: [:dashboard, :feed]
 
   def home
     # J'assigne à ma variable d'instance "@user" le "current_user"
