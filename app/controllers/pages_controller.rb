567256e7aa403d5203df87c97fc30dcb394102e9
Gautier de MAUROY <gautier@de-mauroy.fr>
2025-03-19 09:50:41 +0100

    Remove unnecessary authentication skips in PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: []
-  before_action :authenticate_user!, only: [:dashboard, :feed]
 
   def home
     # J'assigne à ma variable d'instance "@user" le "current_user"
