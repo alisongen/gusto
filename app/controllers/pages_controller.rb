1d93e754ba05a2a335300a7873f5981c869d60b1
GautierDavid <gautier.d.91@gmail.com>
2025-03-21 12:14:16 +0100

    remove useless instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -34,22 +34,6 @@ class PagesController < ApplicationController
     # Récupération des collections de mon @user
     @collections = @user.collections
     @collection = Collection.new
-
-    @colors = [
-      "#4B3B47", # Eggplant
-      "#FFBD33", # Orange
-      "#FFD133", # Yellow
-      "#A3E635", # Lime Green
-      "#33FF57", # Green
-      "#33FFBD", # Mint
-      "#33D1FF", # Sky Blue
-      "#3357FF", # Blue
-      "#8A33FF", # Purple
-      "#FF33F0", # Pink
-      "#FF3380", # Hot Pink
-      "#FF3366"  # Red
-    ]
-    @emojis = [ "🍔", "🥞", "🧁", "🍣", "☕️", "🥗", "🍻", "🥩", "🌯", "💙", "🎉", "🍽️" ]
   end
 
   def switch_window
